63767a1d54acad798aa6765af64147499bcf009b
Kyrylo <kyrylo.onufriiev@gmail.com>
2018-03-18 04:15:58 +0200

    test

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -120,7 +120,7 @@ export async function setContext(app, context) {
       payload: context.payload,
       error: context.error,
       base: '/',
-      env: {}
+      env: {"HOST":"0.0.0.0","PORT":3000}
     }
     // Only set once
     if (context.req) app.context.req = context.req
